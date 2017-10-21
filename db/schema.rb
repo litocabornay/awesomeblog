86e3f5c5f0646ba4ea6a9b36de4f71e4fa4f08ce
Odysseus Ambut <ody@web-mech.net>
2017-10-21 12:51:55 +0800

    updated new machine page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018073925) do
+ActiveRecord::Schema.define(version: 20171020070622) do
 
   create_table "adds", force: :cascade do |t|
     t.datetime "created_at", null: false
